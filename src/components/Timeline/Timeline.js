afcf5231f80ca653eefbe267b4e9da7a53acca97
Alexander Richards <>
2023-04-11 02:12:51 -0500

    Timeline fixes

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -6,8 +6,19 @@ const Timeline = ({ items }) => {
   const timelineRef = useRef(null);
   const [timelineHeight, setTimelineHeight] = useState(0);
 
+  const parseDate = (date) => {
+    if (date === "Present") {
+      return new Date();
+    }
+    return new Date(date);
+  };
+
+  const getSortedItems = () => {
+    return items.sort((a, b) => (parseDate(a.endDate) - parseDate(b.endDate)) * -1);
+  };
+
   const calculatePositions = () => {
-    const sortedItems = items.sort((a, b) => (new Date(a.endDate) - new Date(b.endDate)) * -1);
+    const sortedItems = getSortedItems();
   
     const positions = sortedItems.map((item, index) => {  
       return index % 2 === 0 ? 'left' : 'right';
@@ -15,60 +26,103 @@ const Timeline = ({ items }) => {
   
     return positions;
   };
+
+  const generateDistinctColors = (count) => {
+    const colors = [];
+    const step = 360 / count;
+  
+    for (let i = 0; i < count; i++) {
+      const hue = i * 2 * step;
+      const hslColor = `hsl(${hue}, 100%, 40%)`;
+      colors.push(hslColor);
+    }
+  
+    return colors;
+  };
+
+  const calculateOffsetIndices = (items, positions) => {
+    const offsetIndices = Array(items.length).fill(0);
+    for (let i = 0; i < items.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (positions[i] === positions[j]) {
+          const iEndDate = parseDate(items[i].endDate);
+          const jEndDate = parseDate(items[j].endDate);
+          if (iEndDate <= jEndDate && iEndDate >= parseDate(items[j].startDate)) {
+            offsetIndices[i] = Math.max(offsetIndices[i], offsetIndices[j] + 1);
+          }
+        }
+      }
+    }
+    return offsetIndices;
+  };
+  
   
 
   const calculateDuration = () => {
     const minStartDate = Math.min(...items.map(item => new Date(item.startDate).getTime()));
-    const maxEndDate = Math.max(...items.map(item => new Date(item.endDate).getTime()));
+    const maxEndDate = Math.max(...items.map(item => parseDate(item.endDate).getTime()));
     return maxEndDate - minStartDate;
   };
 
+  const calculateIndividualDuration = (startDate, endDate) => {
+    const relativeStartPos = timelineHeight * calculateRelativePosition(startDate);
+    const relativeEndPos = timelineHeight * calculateRelativePosition(endDate);
+    const absDiff = Math.abs(relativeEndPos - relativeStartPos);
+    return absDiff;
+  }
+
   const calculateRelativePosition = (endDate) => {
     const totalDuration = calculateDuration();
-    const maxEndDate = Math.max(...items.map(item => new Date(item.endDate).getTime()));
-    const relativePosition = (maxEndDate - new Date(endDate).getTime()) / totalDuration;
+    const maxEndDate = Math.max(...items.map(item => parseDate(item.endDate).getTime()));
+    const relativePosition = (maxEndDate - parseDate(endDate).getTime()) / totalDuration;
     return relativePosition;
   };
 
-  const getDateString = (startDate, endDate) => {
+  // Todo: change so it only takes in one date
+  const getDateString = (date) => {
     /* return a string in the form month-start year-start - month-end year-end */
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const startMonth = start.toLocaleString('default', { month: 'long' });
-    const endMonth = end.toLocaleString('default', { month: 'long' });
-    const startYear = start.getFullYear();
-    const endYear = end.getFullYear();
-    return `${startMonth} ${startYear} - ${endMonth} ${endYear}`;
+    const dateObj = new Date(date);
+    const month = dateObj.toLocaleString('default', { month: 'long' });
+    const year = dateObj.getFullYear();
+    return `${month} ${year}`;
   };
   
 
   const positions = calculatePositions();
+  const sortedItems = getSortedItems();
+  const offsetIndices = calculateOffsetIndices(sortedItems, positions);
+  const colors = generateDistinctColors(items.length);
+  const timelineBuffer = 400;
 
   useEffect(() => {
     const timelineElement = timelineRef.current;
     if (timelineElement) {
       const windowHeight = window.innerHeight;
-      const calculatedHeight = Math.max(timelineElement.clientHeight, windowHeight * 2);
+      const calculatedHeight = Math.max(timelineElement.clientHeight, windowHeight * 1.5);
       setTimelineHeight(calculatedHeight);
     }
   }, [timelineRef]);
 
   return (
-    <div className="timeline" ref={timelineRef} style={{ minHeight: timelineHeight }}>
+    <div className="timeline" ref={timelineRef} style={{ minHeight: timelineHeight + timelineBuffer }}>
       <div
         className="timeline-line"
-        style={{ height: `${timelineHeight}px` }}
+        style={{ height: `${timelineHeight + timelineBuffer}px`}}
       ></div>
       {items.map((item, index) => (
         <TimelineItem
           key={index}
           title={item.title}
-          date={`${getDateString(item.startDate, item.endDate)}`}
+          date={`${getDateString(item.startDate)} - ${item.endDate === "Present" ? "Present" : getDateString(item.endDate)}`}
           description={item.description}
+          details={item.details}
           icon={item.icon}
           position={positions[index]}
           relativePosition={calculateRelativePosition(item.endDate)}
           totalHeight={timelineHeight}
+          duration={calculateIndividualDuration(item.startDate, item.endDate)}
+          color={colors[index]}
+          offsetIndex={offsetIndices[index]}
         />
       ))}
     </div>
--- src/components/Timeline/TimelineItem.js
+++ src/components/Timeline/TimelineItem.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import './TimelineItem.css';
 
-const TimelineItem = ({ title, date, description, icon, position, relativePosition, totalHeight }) => {
+const TimelineItem = ({ title, date, description, details, icon, position, relativePosition, totalHeight, duration, color, offsetIndex }) => {
   
-
+  let offset = 11 * offsetIndex;
+  if (offset === 0) {
+    if (position === 'left') {
+      offset = 0;
+    }  
+    else {
+      offset = 4;
+    }
+  }
+  
   return (
     <div
       className={`timeline-item ${position}`}
       style={{ top: `${relativePosition * totalHeight}px` }}
     >
-      <div className="timeline-icon">
+      <div className="timeline-icon" style={{backgroundColor: color}}>
         {icon}
       </div>
-      <div className="timeline-content">
-        <h3>{title}</h3>
-        <h5>{date}</h5>
-        <p>{description}</p>
+      <div className="timeline-info">
+        <div className="timeline-content">
+          <h3>{title}</h3>
+          <h5>{date}</h5>
+          <p>{description}</p>
+          <div className="timeline-details">
+            <p>{details}</p>
+          </div>
+        </div>
       </div>
+      <div
+        className={`timeline-duration ${position}`}
+        style={{ height: `${duration}px`,  backgroundColor: color, [position === 'left' ? 'marginRight' : 'marginLeft']: `${offset}px`}}
+      ></div>
+      
     </div>
   );
 };
