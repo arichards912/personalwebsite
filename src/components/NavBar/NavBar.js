a9df7dd1d3847f35d540bb3e0b22799a72f4d170
Alexander Richards <>
2023-04-19 23:57:22 -0500

    Navbar Hamburger for mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import './NavBar.css';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/" className="nav-logo" activeclassname="active-link" exact="true">
-            AR
-          </NavLink>
-        </li>
+      <NavLink to="/" className="nav-logo" activeclassname="active-link" exact="true">
+        AR
+      </NavLink>
+      <ul className={isMenuOpen ? "nav-links open" : "nav-links"}>
         <li>
           <NavLink to="/resume" activeclassname="active-link">
             RESUME
@@ -32,7 +40,16 @@ function NavBar() {
           </NavLink>
         </li>
       </ul>
+      <Hamburger isOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </nav>
+  );
+}
+
+function Hamburger({ isOpen, toggleMenu }) {
+  return (
+    <div className="hamburger" onClick={toggleMenu}>
+      <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+    </div>
   );
 }
 
